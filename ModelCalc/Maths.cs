32c510b724777eeb59496ae533101e232e7a66b0
PabloZubieta <93717372+PabloZubieta@users.noreply.github.com>
2022-05-04 13:44:21 +0200

    reteste

--- ModelCalc/Maths.cs
+++ ModelCalc/Maths.cs
@@ -9,8 +9,7 @@
         #region public methods
         public int Add(int a, int b)
         {
-            return a + b;
-            
+            return a + b;   
         }
 
         public int Sub(int a, int b)
